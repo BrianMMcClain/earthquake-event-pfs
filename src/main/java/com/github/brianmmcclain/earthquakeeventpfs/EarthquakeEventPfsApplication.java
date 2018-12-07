d3ea3cf7f36902831117f8676eab0c5bf7c30073
Brian McClain <brianmmcclain@gmail.com>
2018-12-06 23:00:42 -0500

    Connect to db

--- src/main/java/com/github/brianmmcclain/earthquakeeventpfs/EarthquakeEventPfsApplication.java
+++ src/main/java/com/github/brianmmcclain/earthquakeeventpfs/EarthquakeEventPfsApplication.java
@@ -1,5 +1,11 @@
 package com.github.brianmmcclain.earthquakeeventpfs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
@@ -12,7 +18,38 @@ public class EarthquakeEventPfsApplication {
 	@Bean
     public Function<String, String> getevents() {
         return s -> {
-			return s.toUpperCase();
+			
+			// Parse environment variables
+			String dbHostEnv = System.getenv("PGHOST");
+			String dbPortEnv = System.getenv("PGPORT");
+			String dbDatabaseEnv = System.getenv("PGDATABASE");
+			String dbUserEnv = System.getenv("PGUSER");
+			String dbPasswordEnv = System.getenv("PGPASSWORD");
+
+			// Process defaults of environment variable isn't defined
+			String dbHost = (dbHostEnv == null) ? "localhost" : dbHostEnv;
+			String dbPort = (dbPortEnv == null) ? "5432" : dbPortEnv;
+			String dbDatabase = (dbDatabaseEnv == null) ? "geocode" : dbDatabaseEnv;
+			String dbUser = (dbUserEnv == null) ? "postgres" : dbUserEnv;
+			String dbPassword = (dbPasswordEnv == null) ? "password" : dbPasswordEnv;
+
+			// Connect to Postgres
+			String dbString = String.format("jdbc:postgresql://%s:%s/%s", dbHost, dbPort, dbDatabase);
+			Properties props = new Properties();
+			props.setProperty("user", dbUser);
+			props.setProperty("password", dbPassword);
+			Connection conn =  null;
+			try {
+				conn = DriverManager.getConnection(dbString, props);
+				Statement statement = conn.createStatement();
+				String queryStatement = "select * from events where timestamp > 'now'::timestamp - '24 hours'::interval;";
+				ResultSet rs = statement.executeQuery(queryStatement);
+			} catch (SQLException e) {
+				// TODO: Process error
+			}
+
+			return "json response";
+			
 		};
     }
 	public static void main(String[] args) {
